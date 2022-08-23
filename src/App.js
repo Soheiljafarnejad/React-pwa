0acc0ce42bc4afe27c6290641eafe0d28843da67
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-08-23 21:05:11 +0430

    Add PWA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-gray-100">
-      <h1>This is Test React App PWA</h1>
+    <div className="w-full h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1 className="text-white font-bold text-2xl">This is Test React App PWA</h1>
     </div>
   );
 }
