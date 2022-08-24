675c689ad4d5d54b511d479a25957bfddeeeb42d
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-08-24 19:57:33 +0430

    Add Button for install PWA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 function App() {
+  let deferredPrompt;
+
+  window.addEventListener("beforeinstallprompt", (e) => {
+    deferredPrompt = e;
+  });
+
+  const clickHandler = async () => {
+    if (deferredPrompt !== null) {
+      deferredPrompt.prompt();
+      const { outcome } = await deferredPrompt.userChoice;
+      if (outcome === "accepted") {
+        deferredPrompt = null;
+      }
+    }
+  };
+
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div className="w-full h-screen flex items-center justify-center flex-col gap-4 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-white font-bold text-2xl">This is Test React App PWA</h1>
+      <button
+        className="px-6 py-1.5 bg-white text-gray-800 rounded-lg font-bold shadow"
+        onClick={clickHandler}
+      >
+        install
+      </button>
     </div>
   );
 }
